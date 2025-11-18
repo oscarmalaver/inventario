8e9f52e69610780a3df47ed25d7a28b53f5619b6
oscarmalaver <oscarjehirmalaverguarin@gmail.com>
2025-11-18 16:06:55 -0500

    frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import categoriaRoutes from "./routes/categorias.routes.js"
+import cors from "cors";
 
 const app = express();
 
 app.set("port",5000)
+
+app.use(cors());
 
 app.use("/api/categorias",categoriaRoutes)
 
